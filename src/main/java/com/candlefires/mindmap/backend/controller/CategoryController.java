948950700b4ace83a0830513acfa40022243d558
Šimon Kocúrek <simon.kocurek@gmail.com>
2019-10-26 23:18:22 +0200

    Add more controllers

---
+++ src/main/java/com/candlefires/mindmap/backend/controller/CategoryController.java
@@ -0,0 +1,10 @@
+package com.candlefires.mindmap.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+}
---
+++ src/main/java/com/candlefires/mindmap/backend/controller/MindMapController.java
@@ -0,0 +1,8 @@
+package com.candlefires.mindmap.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MindMapController {
+
+}
